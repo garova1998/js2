ad95372dca2519baf31d495bd8e31ed94fec705e
Garova1998 <Garova1998@mail.ru>
2021-05-12 13:02:28 +0300

    java

---
+++ js.js
@@ -0,0 +1,11 @@
+alert(16-6);
+var a = 15;
+var b = 5;
+alert(a-b +" вывод с a-b");
+var a1 = 15;
+var b1 = 24;
+var d = 0;
+d = b1-a1 +1;
+document.writeln(d + " Вывод через Writeln");
+console.log(10);
+document.getElementById("vivod").innerHTML = a1 - b + " Вывод через id";
